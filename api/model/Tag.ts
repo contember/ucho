b13589021022272dc3cc982fc9b3c8c8873d7cc9
Jakub Neumann <neumie@neumie.dev>
2025-01-05 18:27:23 +0100

    feat(api): init migration

--- api/model/Tag.ts
+++ api/model/Tag.ts
@@ -1,6 +1,6 @@
 import { c } from '@contember/schema-definition'
-import { projectManagerRole, stakeholderRole, viewerRole, developerRole } from './acl'
 import { FeedbackItem } from './Feedback'
+import { developerRole, projectManagerRole, stakeholderRole, viewerRole } from './acl'
 
 @c.Allow(projectManagerRole, {
 	read: true,
@@ -21,5 +21,6 @@ export class Tag {
 	createdAt = c.dateTimeColumn().notNull().default('now')
 	name = c.stringColumn().notNull()
 	color = c.stringColumn()
+
 	feedbackItems = c.manyHasManyInverse(FeedbackItem, 'tags')
 }
