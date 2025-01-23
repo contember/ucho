5526f9cd756d6cd776cf30fd5288eec721024b90
Jakub Neumann <neumie@neumie.dev>
2025-01-23 23:38:56 +0100

    feat(echo): icons

---
+++ echo/src/components/icons/XCircleIcon.tsx
@@ -0,0 +1,22 @@
+import { Component } from 'solid-js'
+import { IconProps } from '../../types'
+
+export const XCircleIcon: Component<IconProps> = props => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={props.size ?? 24}
+		height={props.size ?? 24}
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke={props.stroke ?? 'currentColor'}
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		class={props.class}
+		style={props.style}
+	>
+		<circle cx="12" cy="12" r="10" />
+		<path d="m15 9-6 6" />
+		<path d="m9 9 6 6" />
+	</svg>
+)
